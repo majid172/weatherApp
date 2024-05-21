60a3403baefaf34c2ec5d0c80201da3851c203f8
majid172 <luthfullahhilmajid172@gmail.com>
2024-05-21 22:51:50 +0600

    api setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-
+import {useWeatherStore} from './stores/weather';
+const weatherStore = useWeatherStore();
 </script>
 
 <template>
@@ -9,7 +10,7 @@
       <div class="col-md-6 mt-5">
         <div class="card p-3 ">
           <h5 class="text-success text-center">Weather App</h5>
-          <input type="text" class="form-control">
+          <input type="text" class="form-control" v-model="weatherStore.location" @keypress="weather.fetachValue">
          <button type="submit" class="btn btn-sm btn-danger mt-3">Submit</button>
         </div>
       </div>
--- src/stores/weather.js
+++ src/stores/weather.js
@@ -1,12 +1,33 @@
 import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
+import axios from 'axios';
 
-export const useWeatherStore = defineStore('weather', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
-})
+export const useWeatherStore = defineStore('weather', {
+  state: () => ({
+    api_key: "4d9d855382ce8b6daed5001c62a8ac71",
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather",
+    location: "",
+    weather: {},
+  }),
+  getters: {},
+  actions: {
+    async fetchWeather() {
+      try {
+        const response = await axios.get(`${this.baseUrl}`, {
+          params: {
+            q: this.location,
+            appid: this.api_key,
+            units: 'metric'
+          }
+        });
+        this.weather = {
+          location: response.data.name,
+          description: response.data.weather[0].description,
+          temperature: response.data.main.temp,
+        };
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    },
+  },
+});
